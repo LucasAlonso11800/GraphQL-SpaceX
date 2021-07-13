7dc348ab222aa41e8795258478860239f7662fbc
Lucas <lucasalonso11800@gmail.com>
2021-07-13 19:54:11 -0300

    Launch component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from 'react';
+// components
+import Launches from './components/Launches';
+// apollo
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
     return (
-        <div>
-            <h1>SpaceX</h1>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="container">
+                <h1>SpaceX</h1>
+                <Launches />
+            </div>
+        </ApolloProvider>
     );
 }
 
